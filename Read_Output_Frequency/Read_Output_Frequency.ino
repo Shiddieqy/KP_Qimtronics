061eb9e8ce3267b39ec134980318fb4976f20147
shiddieqy <shabriashiddieqy@gmail.com>
2023-09-13 20:28:23 +0700

    add read freq

---
+++ Read_Output_Frequency/Read_Output_Frequency.ino
@@ -0,0 +1,21 @@
+#include "FreqCountESP.h"
+
+int inputPin = 14;
+int timerMs = 1000;
+
+void setup()
+{
+  Serial.begin(115200);
+  FreqCountESP.begin(inputPin, timerMs);
+  
+}
+
+void loop()
+{
+  if (FreqCountESP.available())
+  {
+    uint32_t frequency = FreqCountESP.read();
+    Serial.println(frequency);
+  }
+  
+}
